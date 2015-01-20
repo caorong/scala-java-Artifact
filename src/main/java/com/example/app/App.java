2d457c7766ae765a1f85387f565fd87f46115af5
caorong <lelouchcr@gmail.com>
2015-01-20 19:05:17 +0800

    f

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String xx = "{\"map\":null,\"volume\":\"1\"}";
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/scala/com/example/app/Appp.scala
@@ -0,0 +1,10 @@
+package com.example.app
+
+/**
+ * Created by caorong on 15-1-20.
+ */
+object Appp {
+  def main(args: Array[String]) {
+    println("123")
+  }
+}
